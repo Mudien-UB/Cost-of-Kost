68b52fe3ecf2421a19f6c78ae1d713e5693c2750
Mudien-UB <mudienub@gmail.com>
2025-04-30 23:50:39 +0700

    fix header

--- src/dashboard/components/organisms/Header.jsx
+++ src/dashboard/components/organisms/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BiUserCircle } from 'react-icons/bi';
+import { BiBell, BiNotification, BiUserCircle } from 'react-icons/bi';
+import { CgDarkMode } from 'react-icons/cg';
+import { IoMoonOutline } from 'react-icons/io5';
 import { useNavigate } from 'react-router';
 
 export default function Header() {
@@ -22,7 +24,11 @@ export default function Header() {
   return (
     <header className="w-full h-16 px-6 py-4 flex items-center justify-between bg-blue-200 shadow-md fixed top-0 left-0 z-20">
       <h1 className="text-2xl font-bold text-blue-900/50 cursor-default" onClick={handleLogoClick}>{user.name}</h1>
-      <div className="flex gap-10 items-center">
+      <div className="flex gap-10 px-10 items-center">
+        <CgDarkMode className='text-3xl text-blue-900/50 cursor-pointer' />
+        <BiBell className="text-3xl text-blue-900/50 cursor-pointer" 
+          onClick={handleUserClick}
+        />
         <BiUserCircle className="text-3xl text-blue-900/50 cursor-pointer"
           onClick={handleUserClick}
         />
