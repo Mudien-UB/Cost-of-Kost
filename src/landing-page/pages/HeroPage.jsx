e1348cdf1096f282a833316ef3ce919f4c80a260
Mudien-UB <mudienub@gmail.com>
2025-04-30 18:17:46 +0700

    add temporary button on landing page for link to dashboard

--- src/landing-page/pages/HeroPage.jsx
+++ src/landing-page/pages/HeroPage.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { NavLink } from 'react-router'
 
 export default function HeroPage() {
   return (
     <section className="w-screen min-h-screen overflow-x-hidden bg-gray-100 flex items-center justify-center">
-        <div className="bg-white shadow-md rounded-lg text-center p-6 max-w-md w-full">
-            <h1 className="text-2xl text-blue-600 font-bold mb-4">Welcome to the Hero Page</h1>
-            <p className="text-gray-700">This is a simple hero page layout using Tailwind CSS.</p>
-        </div>
-        </section>
+      <div className="bg-white shadow-md rounded-lg text-center p-6 max-w-md w-full">
+        <h1 className="text-2xl text-blue-600 font-bold mb-4">Welcome to the Hero Page</h1>
+        <p className="text-gray-700">This is a simple hero page layout using Tailwind CSS.</p>
+        <NavLink to={'/dashboard'}
+          className="mt-4 inline-block bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition duration-300"
+        >
+          Go to Dashboard
+        </NavLink>
+      </div>
+    </section>
   )
 }
